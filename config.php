e36d45bbea5f3ae4396b4b3a10144243fea6b577
dron rathore <dron.rathore@gmail.com>
2013-07-02 16:40:31 +0530

    commit #3
    New Database Modulo

--- config.php
+++ config.php
@@ -5,9 +5,10 @@
 */
 ini_set("session.name","LightPhp");
 session_start();
-define("TABLE_PREFIX","lp_");
-define("DB_USER_NAME","root");
-define("DB_PASSWORD","fusion");
+define("TABLE_PREFIX", "lp_");
+define("DB_USER_NAME", "root");
+define("DB_PASSWORD", "fusion");
+define("DB_HOST", "127.0.0.1");
 define("PASSWORD_KEY","kSd8D392NLo03N9Gcn9CnXC3NLS");
 define("MY_FOLDER",__dir__);
 define("MY_CLASSES","\lightphp.classes");
--- lightphp.classes/Database.php
+++ lightphp.classes/Database.php
@@ -4,35 +4,42 @@
 	import("Database");
 */
 class Database{
-	private $_db_name,$_connection,$_username,$_password,$_host,$_result,_isInit;
+	private $db_name,$connection,$username,$password,$host,$result;
+	public $isInit;
 	public function __construct($db_name=""){
-		$this->_username=DB_USER_NAME;
-		$this->_password=DB_PASSWORD;
-		$this->_host="localhost";
+		$this->username=DB_USER_NAME;
+		$this->password=DB_PASSWORD;
+		$this->host=DB_HOST;
 		
-		if($this->connect())
-			$this->_isInit=true;
-		else
-			$this->_isInit=false;
-		$this->_db_name=$db_name;
+		if($this->connect()){
+			$this->isInit=true;
+			$this->db_name=$db_name;
+			$this->selectDatabase($this->db_name);
+		}
+		else{
+			$this->isInit=false;
+		}
+	}
+	public function getResult(){
+		return $this->result;
 	}
 	public function selectDatabase($db_name=""){
-		$this->_db_name=strlen($db_name)>0?$db_name:$this->_db_name;
-		mysql_select_db($this->_db_name,$this->_connection);
+		$this->db_name=strlen($db_name)>0?$db_name:$this->db_name;
+		mysql_select_db($this->db_name,$this->connection);
 	}
 	public function switchUser($username,$password=""){
-		$this->_username=$username;
-		$this->_password=$password;
+		$this->username=$username;
+		$this->password=$password;
 	}
 	public function switchHost($host){
-		$this->_host=$host;
+		$this->host=$host;
 	}
 	public function connect(){
 		try{
-			ob_clean();
-			$this->_connection=mysql_connect($this->_host,$this->_username,$this->_password);
-			ob_end_clean();
-			if(!($this->_connection))
+			@ob_clean();
+			$this->connection=mysql_connect($this->host,$this->username,$this->password);
+			@ob_end_clean();
+			if(!($this->connection))
 				throw new Exception();
 			else
 				return true;
@@ -44,32 +51,39 @@ class Database{
 	}
 	public function executeScalar($query){
 		try{
-			ob_clean();
-			$result=mysql_query($query,$this->_connection);
-			ob_end_clean();
+			$result=@mysql_query($query,$this->connection);
 			if(!$result)
 				throw new Exception();
 			else
 				return true;
 		}catch(Exception $e){
-			LightPHP_Error_Log("Error in your Query. ".mysql_error($this->_connection).$e."\r\n");
+			LightPHP_Error_Log("Error in your Query. ".mysql_error($this->connection).$e."\r\n");
 			return false;
 		}
 	}
-	public function query($query,$isarr=false){
+	public function query($query,$onlyOneRecord=false){
 		try{
-			ob_clean();
-			$_result=mysql_query($query);
-			ob_end_clean();
-			if($result){
-				$_result=!($isarr)?mysql_fetch_assoc($_result):mysql_fetch_array($_result);
-				return $_result;
+			$this->result=array();
+			$this->result=@mysql_query($query);
+			if($this->result){
+				if(mysql_num_rows($this->result)>0){
+					if(!$onlyOneRecord){
+						$this->result=mysql_fetch_assoc($this->result);
+					return $this->result;
+					}else{
+						return $this->result;
+					}
+				}
+			}else{
+				throw new Exception();
 			}
-			throw new Exception();
 		}catch(Exception $e){
-			LightPHP_Error_Log("Error in your Query. ".mysql_error($this->_connection).$e."\r\n");
+			LightPHP_Error_Log("Error in your Query. ".mysql_error($this->connection).$e."\r\n");
 		}
 		
 	}
+	public function close(){
+		if($this->isInit)
+			mysql_close($this->connection);
+	}
 }
-?>
